1cdf65bd257cd1515c8c296de3b47f5ce71e8c0b
JungYeonHee <rhdqn1207@naver.com>
2021-01-01 17:43:45 +0900

    jpa 완성

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,25 +1,29 @@
 package hello.hellospring;
 
-import hello.hellospring.repository.JdbcTemplateMemberRepository;
-import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
-import hello.hellospring.repository.jdbcMemberRepository;
+import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
-    private DataSource dataSource;
+    private EntityManager em;
 
     @Autowired
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
+    public SpringConfig(EntityManager em) {
+        this.em = em;
     }
+    // private DataSource dataSource;
+
+    //@Autowired
+    //public SpringConfig(DataSource dataSource) {
+      //  this.dataSource = dataSource;
+    //}
 
     @Bean
     public MemberService memberService(){
@@ -30,6 +34,7 @@ public class SpringConfig {
     public MemberRepository memberRepository(){
         //return new MemoryMemberRepository();
        // return new jdbcMemberRepository(dataSource);
-        return new JdbcTemplateMemberRepository(dataSource);
+      //  return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
     }
 }
