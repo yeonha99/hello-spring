3ade169a32c2e4130469a8742f69d4071e0fd844
JungYeonHee <rhdqn1207@naver.com>
2021-01-02 16:02:25 +0900

    AOP 적용

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -23,16 +23,16 @@ public class MemberService {
 
     //회원가입
     public Long join(Member member) {
-        long start = System.currentTimeMillis();
-        try {
+       //long start = System.currentTimeMillis();
+       // try {
             validateDuplicateMember(member);//중복회원 검증
             memberRepository.save(member);
             return member.getId();
-        }finally {
-            long finish=System.currentTimeMillis();
-            long timeMs=finish-start;
-            System.out.println("join = "+timeMs+"ms");
-        }
+        //}finally {
+         //   long finish=System.currentTimeMillis();
+           // long timeMs=finish-start;
+           // System.out.println("join = "+timeMs+"ms");
+       // }
     }
     private void validateDuplicateMember(Member member) {
         memberRepository.findByName(member.getName())
@@ -41,14 +41,14 @@ public class MemberService {
         });
     }
     public List<Member> findMembers(){
-        long start = System.currentTimeMillis();
-        try {
+       // long start = System.currentTimeMillis();
+        //try {
             return memberRepository.findAll();
-        }finally {
-            long finish=System.currentTimeMillis();
-            long timeMs=finish-start;
-            System.out.println("join = "+timeMs+"ms");
-        }
+       // }finally {
+         //   long finish=System.currentTimeMillis();
+           // long timeMs=finish-start;
+           // System.out.println("join = "+timeMs+"ms");
+       // }
 
     }
     public Optional<Member> findOne(Long memberid){
